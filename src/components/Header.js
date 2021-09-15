b1d24ad6353755873ba6f06da3c88ea82dca4cd6
John <john.gabriel@fatecitapetininga.edu.br>
2021-09-15 17:46:33 -0300

    Projeto React

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div>
+            <h1 style={{color: '#eee'}}>TO-DO List</h1>
+        </div>
+     );
+}
+ 
+export default Header;
---
+++ src/components/TaskDetails.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import Button from './Button';
+
+import './TaskDetails.css'
+
+ const TaskDetails = () => {
+     const params = useParams();
+     const history = useHistory();
+
+     const handleBackButtonClick = () =>{
+         history.goBack();
+     }
+     return ( 
+         <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+                <h2>{params.taskTitle}</h2>
+                <p>{params.taskTitle}</p>
+            </div>
+         </>
+      );
+ }
+  
+ export default TaskDetails;
